cac5b97c05e01f13a88875a78454b68a92cb3e7e
devMonkey87 <jlandetemunoz@icloud.com>
2022-06-06 21:48:04 +0200

    :recycle:

--- src/main/java/com/example/postgresdemo/dto/QuestionDTO.java
+++ src/main/java/com/example/postgresdemo/dto/QuestionDTO.java
@@ -5,15 +5,15 @@ import lombok.Data;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Data
 public class QuestionDTO extends AuditModelDTO {
 
     private int id;
-
     private String title;
-
     private String description;
+    private List<AnswerDTO> answers;
 
 
 }
