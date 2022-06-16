a21bdd3afff5180d840b765283c687fe991d462e
devMonkey87 <jlandetemunoz@icloud.com>
2022-06-16 21:01:55 +0200

    :100: fix issues with mapstruct images

--- src/main/java/com/example/postgresdemo/mapper/QuestionMapper.java
+++ src/main/java/com/example/postgresdemo/mapper/QuestionMapper.java
@@ -3,24 +3,23 @@ package com.example.postgresdemo.mapper;
 import com.example.postgresdemo.Utils;
 import com.example.postgresdemo.dto.QuestionDTO;
 import com.example.postgresdemo.model.Question;
-import jdk.internal.joptsimple.internal.Strings;
 import org.mapstruct.*;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.Base64;
 
 @Mapper(componentModel = "spring", nullValueCheckStrategy = NullValueCheckStrategy.ON_IMPLICIT_CONVERSION)
 public interface QuestionMapper {
 
+    @Named("toBase64")
+    public static String toBase64(byte[] image) {
+        return Utils.decodeByteArrayToBase64(image);
+    }
+
+    @Mappings({
+            @Mapping(source = "image", target = "image", ignore = true)
+    })
     Question toModel(QuestionDTO dto, @Context CycleAvoidingMappingContext context);
 
     @Mappings({
             @Mapping(target = "image", source = "image", qualifiedByName = "toBase64"),
     })
     QuestionDTO toDto(Question model, @Context CycleAvoidingMappingContext context);
-
-    @Named("toBase64")
-    default String toBase64(Question question) {
-        return Utils.decodeByteArrayToBase64(question.getImage());
-    }
 }
